237952e206efa15e93bbe83f56984de07c70770f
newtonmigosi@gmail.com <newtonmigosi@gmail.com>
2018-04-15 16:11:26 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -3,7 +3,10 @@ var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
 function addElementToBeginningofArray(arr, elem) {
   return [elem, ...arr]
 }
+
 function destructivelyAddElementToBeginningofArray(arr, elem) {
   arr.unshift(elem);
   return arr
 }
+
+function addElementToEndOfArray()
